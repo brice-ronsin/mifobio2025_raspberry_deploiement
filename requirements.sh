03202888eea4620cd4db81871ca8409d2fc5b445
brice-ronsin <brice.ronsin@univ-tlse3.fr>
2025-09-05 11:07:53 +0200

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pip install --upgrade pip
+
+pip install absl-py==2.1.0
+pip install colorzero==2.0
+pip install gpiozero==2.0.1
+
+pip install lgpio==0.2.2.0
+pip install numpy==1.20.0
+pip install opencv-python==4.5.3.56
+
+pip install RPi.GPIO==0.7.1
+pip install rpi-lgpio==0.6
+pip install setuptools==44.1.1
+pip install tflite-runtime==2.11.0
+pip install zipp==3.23.0
